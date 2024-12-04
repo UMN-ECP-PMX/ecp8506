24c904e7412a0b6d4406b3337f1a5ff7de43e5de
Kyle Baron <kyleb@metrumrg.com>
2024-12-04 00:20:42 -0600

    add install script

---
+++ wk14/install.R
@@ -0,0 +1,4 @@
+install.packages("remotes")
+remotes::install_github("metrumresearchgroup/pmforest", upgrade = "never", force = TRUE)
+remotes::install_github("metrumresearchgroup/yspec", upgrade = "never", force = TRUE)
+install.packages("mrgsim.sa")
